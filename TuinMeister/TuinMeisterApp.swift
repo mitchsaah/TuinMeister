97d496f07360890958592646c3a681a6712ae1c9
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2025-05-04 14:35:47 +0200

    Initial Commit

---
+++ TuinMeister/TuinMeisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuinMeisterApp.swift
+//  TuinMeister
+//
+//  Created by Mitch Saah on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TuinMeisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
