7023828b7da10ae2dfc472f0400c1fc7a26deee6
mitchsaah <mitch.saah@student.ehb.be>
2025-05-04 15:53:21 +0200

    Added Firebase Init code

--- TuinMeister/TuinMeisterApp.swift
+++ TuinMeister/TuinMeisterApp.swift
@@ -1,14 +1,20 @@
-//
-//  TuinMeisterApp.swift
-//  TuinMeister
-//
-//  Created by Mitch Saah on 04/05/2025.
-//
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct TuinMeisterApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
