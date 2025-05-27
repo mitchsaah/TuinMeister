a6299de1607c02743cae30242ffe974bec13f4da
mitchsaah <mitch.saah@student.ehb.be>
2025-05-27 23:57:55 +0200

    Added the OpenWeatherAPI key and location-based URL builder

--- TuinMeister/ViewModels/Weather/WeatherViewModel.swift
+++ TuinMeister/ViewModels/Weather/WeatherViewModel.swift
@@ -5,4 +5,15 @@ class WeatherViewModel: ObservableObject {
     @Published var temperature: Int = 0
     @Published var humidity: Int = 0
     @Published var uvIndex: Int = 0
+    
+    private let apiKey: String = {
+           return Bundle.main.object(forInfoDictionaryKey: "OPENWEATHER_API_KEY") as? String ?? ""
+       }()
+
+       func fetchWeather(for location: CLLocation) {
+           let lat = location.coordinate.latitude
+           let lon = location.coordinate.longitude
+
+           let url = "https://api.openweathermap.org/data/3.0/onecall?lat=\(lat)&lon=\(lon)&exclude=minutely,hourly,daily,alerts&units=metric&appid=\(apiKey)"
+       }
 }
