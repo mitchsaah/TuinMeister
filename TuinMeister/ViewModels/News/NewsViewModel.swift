202c4f547937febcf87dbb4ae797bbc40b34dda9
mitchsaah <mitch.saah@student.ehb.be>
2025-05-29 22:38:11 +0200

    Added a fetchArticles function to retrieve data from Firestore

--- TuinMeister/ViewModels/News/NewsViewModel.swift
+++ TuinMeister/ViewModels/News/NewsViewModel.swift
@@ -4,4 +4,22 @@ import FirebaseFirestore
 class NewsViewModel: ObservableObject {
     @Published var articles: [NewsArticle] = []
     private var db = Firestore.firestore()
+    
+    func fetchArticles() {
+        db.collection("articles")
+            .order(by: "date", descending: true)
+            .limit(to: 4)
+            .addSnapshotListener { snapshot, error in
+                if let error = error {
+                    print(" Error fetching articles: \(error.localizedDescription)")
+                    return
+                }
+
+                guard let documents = snapshot?.documents else {
+                    print("No documents found")
+                    return
+                }
+
+            }
+    }
 }
