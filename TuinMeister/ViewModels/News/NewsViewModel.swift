4f9d7abdbbf86e4cff5e772f5fd7d22a36fb2439
mitchsaah <mitch.saah@student.ehb.be>
2025-05-29 23:09:13 +0200

    Parsing Firestore documents

--- TuinMeister/ViewModels/News/NewsViewModel.swift
+++ TuinMeister/ViewModels/News/NewsViewModel.swift
@@ -19,7 +19,30 @@ class NewsViewModel: ObservableObject {
                     print("No documents found")
                     return
                 }
+                
+                self.articles = documents.compactMap { doc in
+                    let data = doc.data()
 
+                    guard
+                        let title = data["title"] as? String,
+                        let text = data["text"] as? String,
+                        let imageUrl = data["imageUrl"] as? String,
+                        let source = data["source"] as? String,
+                        let timestamp = data["date"] as? Timestamp
+                    else {
+                        print("Failed to decode an article")
+                        return nil
+                    }
+
+                    return NewsArticle(
+                        id: doc.documentID,
+                        title: title,
+                        text: text,
+                        imageUrl: imageUrl,
+                        source: source,
+                        date: timestamp.dateValue()
+                    )
+                }
             }
     }
 }
