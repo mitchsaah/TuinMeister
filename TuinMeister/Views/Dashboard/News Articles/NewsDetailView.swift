265cebc3792ba19f1788f749460dd6ef34fd93e3
mitchsaah <mitch.saah@student.ehb.be>
2025-05-29 23:27:59 +0200

    Added a NewsDetailView with scrollview

---
+++ TuinMeister/Views/Dashboard/News Articles/NewsDetailView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct NewsDetailView: View {
+    let article: NewsArticle
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+            }
+            .padding()
+        }
+        .navigationTitle("")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
