c6ad7f162233edf04b4e4a15cecfb11151a8a557
mitchsaah <mitch.saah@student.ehb.be>
2025-05-30 00:42:57 +0200

    Added a section title for the news article

--- TuinMeister/Views/Dashboard/News Articles/NewsSection.swift
+++ TuinMeister/Views/Dashboard/News Articles/NewsSection.swift
@@ -6,6 +6,16 @@ struct NewsSection: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
+            // Section title
+            HStack(spacing: 0) {
+                Text("Nieuws")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text(" Blad")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hex: 0x7FC241))
+            }
         }
         .padding(.horizontal, 8)
         .onAppear {
