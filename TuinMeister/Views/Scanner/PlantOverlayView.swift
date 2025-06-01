cd1448879233411812d735f0d5310c77eecbd33e
mitchsaah <mitch.saah@student.ehb.be>
2025-06-02 01:38:09 +0200

    Created a basic structure for PlantOverlayView - This is for when a plant has been scanned and then the info pops up in an overlay

---
+++ TuinMeister/Views/Scanner/PlantOverlayView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct PlantOverlayView: View {
+    let suggestion: Suggestion
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+            }
+            .padding()
+        }
+    }
+}
