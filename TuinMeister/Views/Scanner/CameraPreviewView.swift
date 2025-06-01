0c258e5901dd606489f9d69f184d2901098bdc16
mitchsaah <mitch.saah@student.ehb.be>
2025-06-02 00:52:21 +0200

    Added a CameraPreviewView for displaying live AVCaptureSession feed

---
+++ TuinMeister/Views/Scanner/CameraPreviewView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+import AVFoundation
+
+struct CameraPreviewView: UIViewRepresentable {
+    let session: AVCaptureSession
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        let layer = AVCaptureVideoPreviewLayer(session: session)
+        layer.videoGravity = .resizeAspectFill
+        layer.frame = UIScreen.main.bounds
+        view.layer.addSublayer(layer)
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
