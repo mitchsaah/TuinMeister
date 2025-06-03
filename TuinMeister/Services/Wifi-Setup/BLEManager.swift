055f9095979dbbc449713cbe013e005721b6259d
mitchsaah <mitch.saah@student.ehb.be>
2025-06-03 13:51:39 +0200

    Implemented BLE scanning with restartScan logic - Required state handling - Implemented restartScan() to begin scanning when Bluetooth is powered on - Included serviceUUID for filtered BLE scanning - Triggered scan from centralManagerDidUpdateState when state is poweredOn

--- TuinMeister/Services/Wifi-Setup/BLEManager.swift
+++ TuinMeister/Services/Wifi-Setup/BLEManager.swift
@@ -2,16 +2,36 @@ import Foundation
 import CoreBluetooth
 
 class BLEManager: NSObject, ObservableObject {
+    @Published var peripherals: [CBPeripheral] = []
+    @Published var isScanning = false
+
     private var centralManager: CBCentralManager!
+    
+    private let serviceUUID  = CBUUID(string: "ABF00000-0000-0000-0000-000000000000")
 
     override init() {
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func restartScan() {
+        guard centralManager.state == .poweredOn else {
+            print("[BLEManager] Bluetooth inactive, no scanning available.")
+            return
+        }
+        peripherals.removeAll()
+        isScanning = true
+        print("[BLEManager] Start BLE-scan...")
+        centralManager.scanForPeripherals(withServices: [serviceUUID], options: nil)
     }
 }
 
 extension BLEManager: CBCentralManagerDelegate {
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         print("[BLEManager] Bluetooth state: \(central.state.rawValue)")
+        
+        if central.state == .poweredOn {
+            restartScan()
+        }
     }
 }
