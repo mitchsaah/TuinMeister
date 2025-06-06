0a622480a3a17517bee327acd8ee78b656f263e5
mitchsaah <mitch.saah@student.ehb.be>
2025-06-06 15:50:53 +0200

    Added saveDeviceData function with required parameters

--- TuinMeister/Services/Save Plant Details/DeviceDataService.swift
+++ TuinMeister/Services/Save Plant Details/DeviceDataService.swift
@@ -6,4 +6,14 @@ class DeviceDataService {
     static let shared = DeviceDataService()
 
     private init() {}
+    
+    func saveDeviceData(
+            deviceName: String,
+            plantName: String,
+            customName: String,
+            type: String,
+            plantDate: Date,
+            imageUrl: String,
+            completion: @escaping (Error?) -> Void
+    ) {}
 }
