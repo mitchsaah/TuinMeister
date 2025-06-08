b52c5aa89d4fe56c4d72473d0a50a7f72c0fef29
mitchsaah <mitch.saah@student.ehb.be>
2025-06-08 22:46:21 +0200

    Added a DeviceCardView

---
+++ TuinMeister/Views/Dashboard/DeviceCardView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct DeviceCardView: View {
+    let device: Device
+    @Environment(\.colorScheme) private var colorScheme
+    private let accentGreen = Color(hex: 0x7FC241)
+    
+    var body: some View {
+        VStack(spacing: 8) {
+        }
+    }
+}
