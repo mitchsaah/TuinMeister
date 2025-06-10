2aca7bdbdfd7232dcbb6f7218fefb5655177c3bf
mitchsaah <mitch.saah@student.ehb.be>
2025-06-10 21:29:22 +0200

    Added a Firebase references (db and uid) - FirebaseAuth for retrieving the uid - FirebaseFirestore for access to Firestore

--- TuinMeister/ViewModels/PlantID/ArchiveViewModel.swift
+++ TuinMeister/ViewModels/PlantID/ArchiveViewModel.swift
@@ -1,8 +1,13 @@
 import Foundation
 import Combine
+import FirebaseAuth
+import FirebaseFirestore
 
 final class ArchiveViewModel: ObservableObject {
     static let shared = ArchiveViewModel()
 
     @Published private(set) var archived: [ArchivedPlant] = []
+    
+    private let db = Firestore.firestore()
+    private var uid: String? { Auth.auth().currentUser?.uid }
 }
