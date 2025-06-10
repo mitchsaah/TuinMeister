530ac8176c141dedde4aecdc0fb3243c310fa608
mitchsaah <mitch.saah@student.ehb.be>
2025-06-10 23:18:31 +0200

    Added a VStack with family, and maintenance => Also implemented the plant name

--- TuinMeister/Views/Profile/Plant Archive/ArchivedPlantCardView.swift
+++ TuinMeister/Views/Profile/Plant Archive/ArchivedPlantCardView.swift
@@ -11,7 +11,22 @@ struct ArchivedPlantCardView: View {
                 .frame(width: 80, height: 80)
                 .foregroundColor(.gray)
             
-            Text(plant.name)
+            VStack(alignment: .leading, spacing: 4) {
+                Text(plant.name)
+                    .font(.headline)
+                HStack {
+                    Text("Familie:")
+                        .font(.caption).bold()
+                    Text(plant.family ?? "Onbekend")
+                        .font(.caption)
+                }
+                HStack {
+                    Text("Onderhoud:")
+                        .font(.caption).bold()
+                    Text(plant.maintenance ?? "-")
+                        .font(.caption)
+                }
+            }
         }
     }
 }
